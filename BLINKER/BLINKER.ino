68e040e1b7976caf7561b43594fca4302ead79ee
youmna ahmed <you@example.com>
2022-09-07 16:39:13 +0200

    second edit

---
+++ BLINKER/BLINKER.ino
@@ -0,0 +1,12 @@
+void setup() {
+pinMode(9,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(9,HIGH);
+  delay(1000);
+  digitalWrite(9,LOW);
+  delay(1000);
+}
